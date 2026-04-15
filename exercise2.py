02511e2f5d187666b2591c59ee315dc43e275205
wendamys <wendamys@yandex.ru>
2026-04-15 15:16:09 +0700

    add exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,31 @@
+
+class Time:
+    def user_input(self):
+        while True:
+            user_input = input()
+            try:
+                seconds = int(user_input)
+                if seconds >= 0:
+                    return seconds
+                print("Incorrect time")
+                return None
+            except ValueError:
+                print("Could not parse a number. Please, try again")
+
+    def search_time_hh_mm_ss(self, seconds):
+        hour = seconds // 3600
+        seconds %= 3600
+        minutes = seconds // 60
+        seconds %= 60
+        Array = [hour, minutes, seconds]
+        return Array
+
+    def print_time(self, Array):
+        print(f"{Array[0]:02}:{Array[1]:02}:{Array[2]:02}")
+
+
+Time_obj = Time()
+second = Time_obj.user_input()
+if second is not None:
+    myArray = Time_obj.search_time_hh_mm_ss(second)
+    Time_obj.print_time(myArray)
