1c38d8707c80f6236b1745c62d82944314e99701
wendamys <wendamys@yandex.ru>
2026-04-22 11:09:26 +0700

    add Exercise8.py

--- src/Exercise8.py
+++ src/Exercise8.py
@@ -4,28 +4,19 @@ class Exercise8:
         pred_vvod = float('-inf')
         k = 0
         while True:
-            user_in = input()
+            user_in = input().split()
             try:
                 for x in user_in:
                     if x == " ":
                         pass
                     else:
-                        try:
-                            user_in = int(x)
-                            if pred_vvod == float('-inf'):
-                                pred_vvod = user_in
-                            elif user_in > pred_vvod:
-                                pred_vvod = user_in
-                            else:
-                                print(f"The sequence is not ordered from the ordinal number of the number {k}")
-                                return
-                            k += 1
-                        except ValueError:
-                            if pred_vvod != float('-inf'):
-                                print("The sequence is ordered in ascending order")
-                                return
-                            print("Input error")
+                        user_in = int(x)
+                        if user_in > pred_vvod:
+                            pred_vvod = user_in
+                        else:
+                            print(f"The sequence is not ordered from the ordinal number of the number {k}")
                             return
+                        k += 1
             except ValueError:
                 if pred_vvod != float('-inf'):
                     print("The sequence is ordered in ascending order")
